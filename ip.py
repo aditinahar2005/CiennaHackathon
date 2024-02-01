aac6370ad9f3c58041197e2fb430714c95b8b023
Khush Dinesh Golani <67571400+khush938@users.noreply.github.com>
2024-02-01 18:59:00 -0500

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,25 @@
+# Ciena Coding Challenge.
+
+import time
+
+class IPAdress:
+    adress = ''
+    validLease = True # True means u can connect, false means its in use.
+    startTimer =  0.0
+    # userID = 
+
+    def __init__(self, adress, validLease):
+        self.adress = adress
+        self.validLease = False
+        self.startTimer = time.time()
+
+    def release(self):
+        self.validLease = True
+        print("IP address released sucessfully.")
+
+    def isValidLease(self):
+        if (time.time() - self.startTimer > 60):
+            return False
+        else:
+            return True
+    # def leaseTimer(self):
