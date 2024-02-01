695ef3674ce411d4282373ec19fee98ee21e2d28
Akshatjhawar7 <119635197+Akshatjhawar7@users.noreply.github.com>
2024-02-01 15:49:06 -0800

    Add files via upload

--- Challenge.py
+++ Challenge.py
@@ -1,11 +1,13 @@
-def assign(){
+def assign():
     IP = generate()
-    for i in range(0,list.length):
-        if(list[i].adress == IP):
-            if(isValidLease()):
-                assign()
+    find(IP)
     IPaddr = IPAdress(IP,False)
     Ipaddr.time()
     list.append(IP)
     return Iaddr
-}
+def find(IP):
+    for i in range(0,list.length):
+        if(list[i].adress == IP):
+            if(isValidLease()):
+                assign()
+    return IP
